6520cf1df243f20e149cba616f55db9369da1291
김종식 <otw1917@naver.com>
2023-01-26 18:15:48 +0900

    commit

--- src/main/java/com/example/batisproject/controller/AuthenticationForModel.java
+++ src/main/java/com/example/batisproject/controller/AuthenticationForModel.java
@@ -1,5 +1,6 @@
 package com.example.batisproject.controller;
 
+import com.example.batisproject.entity.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,14 @@ import org.springframework.stereotype.Component;
 public class AuthenticationForModel {
 
     // 해당 메소드는 사용자 정보를 들고 오기 위함으로 만들어진 메소드임.
-    public Authentication getAuthentication() {
-        return SecurityContextHolder.getContext().getAuthentication();
+    public User getAuthentication() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.isAuthenticated()) {
+            return(User) authentication.getPrincipal();
+        }
+
+
+        return null;
     }
 }
