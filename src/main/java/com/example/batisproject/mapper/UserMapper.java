6715e52607a04495081df0e96d46680977559e01
devjun <devjun63@gmail.com>
2023-01-26 16:38:04 +0900

    commit

--- src/main/java/com/example/batisproject/mapper/UserMapper.java
+++ src/main/java/com/example/batisproject/mapper/UserMapper.java
@@ -11,7 +11,9 @@ public interface UserMapper {
     @Select("Select * from User")
     @Results(id = "userMap", value = {
             @Result(property = "id", column = "u_id"),
-            @Result(property = "username", column = "email"),
+            @Result(property = "category", column = "c_id"),
+            @Result(property = "location", column = "l_id"),
+            @Result(property = "username", column = "username"),
             @Result(property = "password", column = "password"),
             @Result(property = "nickname", column = "nickname"),
             @Result(property = "regdate", column = "regdate"),
@@ -20,21 +22,36 @@ public interface UserMapper {
     })
     List<User> getAll();
 
-
-    @Select("select * from user where email = #{email} and password = #{password}")
+    @Select("select * from user where c_id = 1 and l_id = 19000 and #{username}")
     @ResultMap("userMap")
-    User getByEmailPassword(@Param("email") String email, @Param("password") String password);
+    User getByEmailInit(@Param("username") String username);
 
-    @Insert("INSERT INTO user(email, password, nickname, regdate, point) VALUES ( #{user.username}, #{user.password}, #{user.nickname}, NOW(), #{user.point})")
-    @Options(useGeneratedKeys = true, keyProperty = "id") // 입력할 때 생성된 id를 바로 반환함.
-    int insert(@Param("user") User user);
-
-    @Select("select * from user where email = #{email}")
+    @Select("select * from user where email = #{username}")
     @ResultMap("userMap")
-    User findByEmail(@Param("email") String email);
+    User existsByEmail(@Param("username")  String username);
 
     @Select("select * from user where nickname = #{nickname}")
     @ResultMap("userMap")
     User existsByNickName(@Param("nickname") String nickname);
+
+
+    @Select("select * from user where username = #{username} and password = #{password}")
+    @ResultMap("userMap")
+    User getByEmailPassword(@Param("username") String username, @Param("password") String password);
+
+    @Insert("INSERT INTO user(username, password, nickname) VALUES (#{user.username}, #{user.password}, #{user.nickname})")
+    @Options(useGeneratedKeys = true, keyProperty = "id") // 입력할 때 생성된 id를 바로 반환함.
+    int insert(@Param("user") User user);
+
+
+
+    // 회원 정보 수정
+
+    @Update("update user set c_id = #{user.cId}, l_id = #{user.lId}, point = #{user.point}, role = #{user.role}" +
+            " where username = #{user.username}")
+    @ResultMap("userMap")
+    boolean updateUser(@Param("user") User user);
+
+
 
 }
