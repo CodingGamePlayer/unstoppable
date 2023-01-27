ec4c79053ed0d4a3b117cd862fc2820359b78b70
김종식 <otw1917@naver.com>
2023-01-28 02:17:11 +0900

    commit

--- src/main/java/com/example/batisproject/dto/PageRequestDTO.java
+++ src/main/java/com/example/batisproject/dto/PageRequestDTO.java
@@ -22,10 +22,10 @@ public class PageRequestDTO {
     private int page = 1;
 
     @Builder.Default
-    @Min(value = 10)
+    @Min(value = 9)
     @Max(value = 100)
     @Positive
-    private int size = 10;
+    private int size = 9;
 
     private String[] types;
 
@@ -36,6 +36,15 @@ public class PageRequestDTO {
     private LocalDate from;
 
     private LocalDate to;
+
+    private String search;
+
+    private String username;
+
+    private String nickname;
+
+
+
 
     public int getSkip() {
         return (page-1) * 10;
