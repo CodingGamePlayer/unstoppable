1be8bb6c9a98ca9bc4739731e625ab23cba66fa5
CodingGamePlayer <115041502+CodingGamePlayer@users.noreply.github.com>
2023-01-31 11:12:24 +0900

    Jungi (#23)
    * mygather and gather categorySearch method
    
    * commit (#8)
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Haha (#9)
    
    * commit
    
    * commit
    
    * commit
    
    * Kjs (#11)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * Kjs (#14)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * Kjs (#15)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * gatherList and myGatherList and signUp Page
    
    * Kjs (#22)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * init location modal setting
    
    ---------
    
    Co-authored-by: devjun <devjun63@gmail.com>
    Co-authored-by: JunGi Jeong <59526539+devjun63@users.noreply.github.com>

--- src/main/java/com/example/batisproject/controller/user/UserController.java
+++ src/main/java/com/example/batisproject/controller/user/UserController.java
@@ -4,6 +4,7 @@ import com.example.batisproject.annotaion.CurrentUser;
 import com.example.batisproject.controller.AuthenticationForModel;
 import com.example.batisproject.dto.UserDTO;
 import com.example.batisproject.entity.User;
+import com.example.batisproject.service.location.LocationService;
 import com.example.batisproject.service.user.impl.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -19,13 +21,17 @@ public class UserController {
 
     @Autowired
     private UserServiceImpl userService;
+    @Autowired
+    private LocationService locationService;
 
     @GetMapping("/user/main")
     public String main(Model model, @CurrentUser User user) {
 
         UserDTO userDTO = userService.existsByEmail(user.getUsername());
 
+        model.addAttribute("locationList",locationService.getAll());
         model.addAttribute("user", userDTO);
+
 
         return "user/main";
     }
@@ -40,5 +46,15 @@ public class UserController {
         return "user/for-function/user-payment";
     }
 
+    @GetMapping("/user/update/location")
+    public String updateUserLocation(Integer location, @CurrentUser User user, Model model) {
+        System.out.println("locaiton : " + location);
+        UserDTO userDTO = userService.existsByEmail(user.getUsername());
+        userDTO.setLocation(location);
+        int result = userService.updateUserByLocation(userDTO);
+        log.info("result : " + result);
+        model.addAttribute("user", userDTO);
+        return "user/main";
+    }
 
 }
