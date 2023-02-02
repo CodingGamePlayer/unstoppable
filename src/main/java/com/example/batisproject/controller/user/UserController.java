642afd0753a381414f7cb59c30dbd40d25c1b259
JunGi Jeong <59526539+devjun63@users.noreply.github.com>
2023-02-02 13:09:17 +0900

    Jungi (#29)
    * mygather and gather categorySearch method
    
    * commit (#8)
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Haha (#9)
    
    * commit
    
    * commit
    
    * commit
    
    * Kjs (#11)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * Kjs (#14)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * Kjs (#15)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * gatherList and myGatherList and signUp Page
    
    * Kjs (#22)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * init location modal setting
    
    * Jungi (#19)
    
    * mygather and gather categorySearch method
    
    * commit (#8)
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Haha (#9)
    
    * commit
    
    * commit
    
    * commit
    
    * Kjs (#11)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * Kjs (#14)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * Kjs (#15)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    ---------
    
    Co-authored-by: devjun <devjun63@gmail.com>
    
    * Kjs (#22)
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * change dto and entity chatting and chattingMessage to GatherComment and GatherCommentMessage and mygatherlist and gatherlist logics
    
    * change duplicate mapper method
    
    * change duplicate mapper method
    
    * change static SQL to Dynamic SQl
    
    ---------
    
    Co-authored-by: CodingGamePlayer <115041502+CodingGamePlayer@users.noreply.github.com>

--- src/main/java/com/example/batisproject/controller/user/UserController.java
+++ src/main/java/com/example/batisproject/controller/user/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
