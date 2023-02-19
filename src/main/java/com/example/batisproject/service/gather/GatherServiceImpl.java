902f7c43c2e72a8f6c1d2aca1f121fb45e280a20
김종식 <otw1917@naver.com>
2023-02-20 01:13:40 +0900

    Revert "Revert "Jungi (#62)""
    This reverts commit 8d58e689fce2de72badb2f9d383bd78c2f352bd9.

--- src/main/java/com/example/batisproject/service/gather/GatherServiceImpl.java
+++ src/main/java/com/example/batisproject/service/gather/GatherServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.batisproject.service.gather;
 
-import com.example.batisproject.dto.*;
+import com.example.batisproject.dto.GatherDTO;
+import com.example.batisproject.dto.GatherResponseDTO;
+import com.example.batisproject.dto.PageRequestDTO;
+import com.example.batisproject.dto.PageResponseDTO;
 import com.example.batisproject.entity.Gather;
 import com.example.batisproject.entity.GatherResponse;
-import com.example.batisproject.entity.User;
 import com.example.batisproject.mapper.jungi.CategoryMapper;
 import com.example.batisproject.mapper.jungi.GatherMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +13,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,7 +24,13 @@ public class GatherServiceImpl implements GatherService {
 
     private final CategoryMapper categoryMapper;
 
-    private ModelMapper modelMapper = new ModelMapper();
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    @Autowired
+    public GatherServiceImpl(GatherMapper gatherMapper, CategoryMapper categoryMapper) {
+        this.gatherMapper = gatherMapper;
+        this.categoryMapper = categoryMapper;
+    }
 
     @Override
     public List<GatherDTO> getAll() {
@@ -50,7 +56,7 @@ public class GatherServiceImpl implements GatherService {
     public int create(GatherDTO gatherDTO) {
         Gather gather = modelMapper.map(gatherDTO, Gather.class);
         int result = gatherMapper.insert(gather);
-        log.info("insert gather Id : "+gather.getId());
+        log.info("insert gather Id : " + gather.getId());
         log.info("==================================");
         log.info("result : " + result);
         if (result == 0) {
@@ -81,6 +87,7 @@ public class GatherServiceImpl implements GatherService {
         List<GatherResponse> gathers = gatherMapper.selectMyList(pageRequestDTO);
 
         List<GatherResponseDTO> dtoList = gathers.stream()
+
 
                 .map(gatherResponse -> modelMapper.map(gatherResponse, GatherResponseDTO.class))
                 .collect(Collectors.toList());
@@ -121,10 +128,4 @@ public class GatherServiceImpl implements GatherService {
         return categoryMapper.getNamebyCategoryId(category);
     }
 
-
-    @Autowired
-    public GatherServiceImpl(GatherMapper gatherMapper, CategoryMapper categoryMapper) {
-        this.gatherMapper = gatherMapper;
-        this.categoryMapper = categoryMapper;
-    }
 }
